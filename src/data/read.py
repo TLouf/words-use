010fb31fe28cc5d6878bc6705d2b8a94f1d5833d
TLouf <loufthomas@gmail.com>
2021-03-17 18:08:53 +0100

    Adapt read of county data to fit new data

--- src/data/read.py
+++ src/data/read.py
@@ -8,8 +8,9 @@ def distribs_extract(list_fips, freq_file_format):
     calculate global counts.
     '''
     counties_dict = {}
-    global_words = pd.DataFrame({'count': [], 'nr_counties': []},
+    global_words = pd.DataFrame({'count': [], 'nr_cells': []},
                                 index=pd.Index([], name='word'))
+    counties_counts = {'cell_id': [], 'word': [], 'count': []}
     for i, fips in enumerate(list_fips):
         print(f'{i+1} / {len(list_fips)}', end='\r')
         filename = str(freq_file_format).format(fips)
@@ -21,13 +22,18 @@ def distribs_extract(list_fips, freq_file_format):
         word_counts.index = word_counts.index.str.replace(pattern, '')
         word_counts = word_counts.loc[word_counts.index != '']
         word_counts = word_counts.groupby('word', sort=False)['count'].sum()
-        # rather do a set intersection??
+        counties_counts['cell_id'].extend([fips] * word_counts.shape[0])
+        counties_counts['word'].extend(word_counts.index.values)
+        counties_counts['count'].extend(word_counts.values)
+        # All the following variables are not used anymore, but I keep the code
+        # there for now.
         global_words = global_words.join(word_counts.loc[word_counts > 5],
                                          rsuffix='_cnt', how='outer')
         is_in_county = global_words['count_cnt'].notnull()
-        global_words['nr_counties'] = global_words['nr_counties'].fillna(0)
-        global_words.loc[is_in_county, 'nr_counties'] += 1
-        global_words[['count', 'count_cnt']] = global_words[['count', 'count_cnt']].fillna(0)
+        global_words['nr_cells'] = global_words['nr_cells'].fillna(0)
+        global_words.loc[is_in_county, 'nr_cells'] += 1
+        global_words[['count', 'count_cnt']] = (
+            global_words[['count', 'count_cnt']].fillna(0))
         global_words['count'] += global_words['count_cnt']
         global_words = global_words.drop(columns=['count_cnt'])
         grp_rank_counts = word_counts.unique()
@@ -41,4 +47,7 @@ def distribs_extract(list_fips, freq_file_format):
         counties_dict[fips] = {
             'word_counts': word_counts, 'grp_rank_counts': grp_rank_counts,
             'normed_log_ranks': normed_log_ranks, 'freq_distrib': freq_distrib}
-    return global_words, counties_dict
+
+    counties_counts = pd.DataFrame(counties_counts).set_index(['word',
+                                                               'cell_id'])
+    return global_words, counties_dict, counties_counts
