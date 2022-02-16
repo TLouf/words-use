8b228f23c42fb8e9d6560e2a32fb372f17338f49
TLouf <loufthomas@gmail.com>
2022-02-16 17:34:15 +0100

    Allow plotting word cloud in existing ax

--- src/visualization/words.py
+++ src/visualization/words.py
@@ -2,18 +2,24 @@ import matplotlib.pyplot as plt
 import wordcloud
 
 
-def make_word_cloud(word_weights, figsize, save_path=None, dpi=300):
+def cloud(word_weights, figsize=None, save_path=None, dpi=300,
+          fig=None, ax=None, **kwargs):
     '''
     Plot a word cloud with the words given by the keys of `word_weights`, and
     their relative sizes by its values.
     '''
-    fig, ax = plt.subplots(1, figsize=figsize, dpi=dpi)
-    width, height = fig.dpi_scale_trans.transform(figsize)
+    if ax is None:
+        fig, ax = plt.subplots(1, figsize=figsize, dpi=dpi)
+    fig = ax.get_figure()
+
+    bbox = ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+    width, height = bbox.width * fig.dpi, bbox.height * fig.dpi
+    # width, height = fig.dpi_scale_trans.transform(fig.get_size_inches())
     cloud_kwargs = {
         'background_color': 'white', 'scale': width/600, 'colormap': 'Dark2',
-        'width': 600, 'height': 300
+        'width': int(width), 'height': int(height), **kwargs
     }
-    cloud = wordcloud.WordCloud(**cloud_kwargs).generate_from_frequencies(word_weights)
+    cloud = wordcloud.WordCloud(**cloud_kwargs).generate_from_frequencies(dict(word_weights))
     _ = ax.imshow(cloud, interpolation='bilinear')
     ax.set_axis_off()
     
